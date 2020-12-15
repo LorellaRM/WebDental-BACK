95a8c500a5100154f16e9dddad6799845cc608f3
LorellaRM <lorella.1892@gmail.com>
2020-12-15 17:46:33 +0000

    base de datos

--- src/models.py
+++ src/models.py
@@ -2,62 +2,66 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Users(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(200), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    name = db.Column(db.String(200), unique=False, nullable=False)
-    lastname = db.Column(db.String(250), unique=False, nullable=False)
-    phone_Number = db.Column(db.Integer(9), unique=False, nullable=False)
-    city = db.Column(db.String(200), unique=False, nullable=False)
-    adress = db.Column(db.String(200), unique=False, nullable=False)
-    cipcode = db.Column(db.Integer(5), unique=False, nullable=False)
-    
+    password = db.Column(db.String(80), nullable=False)
+    phone_number = db.Column(db.String(9), nullable=False)    
+    city = db.Column(db.String(200), nullable=False)
+    address = db.Column(db.String(200), nullable=False)
+    zipcode = db.Column(db.String(5), nullable=False)
+    is_active = db.Column(db.Boolean, nullable=False) 
 
-
+    appointments = db.relationship("Appointments")   
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.first_name
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+
             # do not serialize the password, its a security breach
         }
 
-class Tratment(db.Model):
+class Tratments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    dental_Tratment = db.Column(db.Enum("Revisión", "Presupuesto", "Limpieza", "Empaste", "Endodoncia", "Ortodoncia", "Revisión Ortodoncia", "Implante", "Revisión Implante"), unique=True, nullable=False)
-    city = db.Column(db.String(200), unique=False, nullable=False)
-    adress = db.Column(db.String(200), unique=False, nullable=False)
-    cipcode = db.Column(db.Integer(5), unique=False, nullable=False)
+    title = db.Column(db.String(120), unique=True, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    time = db.Column(db.Integer, nullable=False)
+    amount = db.Column(db.Numeric, nullable=False)
+    currency = db.Column(db.String(3), nullable=False)
+
+    appointments = db.relationship("Appointments")
     
-
-
-
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Tratment %r>' % self.title
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "title": self.title,
+
             # do not serialize the password, its a security breach
         }
 
-class Appointment(db.Model):
+
+class Appointments(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    id_Tratment = db.Column(db.Integer)
-    id_User = db.Column(db.Integer)
-    date = db.Column(db.Datetime.datetime(), unique=False, nullable=False)
-    
+    tratment_id = db.Column(db.Integer, db.ForeignKey("tratments.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    datetime = db.Column(db.DateTime, nullable=False)
 
-
+    tratment = db.relationship("Tratments")
+    user = db.relationship("Users")
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Appointment %r>' % self.id
 
     def serialize(self):
         return {
